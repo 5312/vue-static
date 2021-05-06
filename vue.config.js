afa8bb10c63e828b1ea9baeef5eee515f23fe578
5312 <1765080897@qq.com>
2021-05-06 15:02:59 +0800

    路径问题

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,15 @@
 module.exports = {
   productionSourceMap: false,
+  publicPath: './',
   css: {
     loaderOptions: {
       postcss: {
         plugins: [
           require('postcss-plugin-px2rem')({
-            rootValue: 75,      // 新版本的是这个值
-            mediaQuery: false, //（布尔值）允许在媒体查询中转换px。
-            minPixelValue: 3 //设置要替换的最小像素值(3px会被转rem)。 默认 0
-          }),
+            rootValue: 75, // 新版本的是这个值
+            mediaQuery: false, // （布尔值）允许在媒体查询中转换px。
+            minPixelValue: 3 // 设置要替换的最小像素值(3px会被转rem)。 默认 0
+          })
         ]
       }
     }
