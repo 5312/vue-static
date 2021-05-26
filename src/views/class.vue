31d3a9f815d1a998e5a1f11964c98a7ccd8805d0
5312 <1765080897@qq.com>
2021-05-26 14:55:08 +0800

    修改文字

--- src/views/class.vue
+++ src/views/class.vue
@@ -30,8 +30,8 @@ export default {
   name: 'Class',
   data () {
     return {
-      text: '课程主要面对从事电视会议系统执勤维护工作的相关人员，针对当前典型电视会议系统设备操作训练需求，包含原理示教、装备操作、作战运用3个组训单元，22个训练科目，重点针对电视会议系统功能、模型、接口、线缆等县相关基础知识、主流电视会议系统的结构组成及工作原理、典型设备操作和配置方法以及组网运用方法进行训练。培养执勤维护人员具备对应岗位职责所需的专业素养、满足信息通信保障任务要求。'
-
+      text:
+        '课程面向电视会议系统值勤维护工作相关人员,聚焦当前典型电视会议系统设备训练需求,围绕原理知识、操作训练、任务实施设置课程内容.包含原理示教、装备操作、作战运用3个组训单元,22 个训练科目，融合军事游戏化思想.提供训练、考核2种模式，提供远程式、虚拟化、交互式训练与考核，旨在提升全军各级视频保障人员岗位技能水平。'
     }
   },
   components: {}
@@ -42,7 +42,7 @@ export default {
 @bg: #1f1f1f;
 .home {
   width: 100%;
-  height:100%;
+  height: 100%;
   position: relative;
   // background:url('../assets/img/class/group.png') 100% 100% / 100% 100% no-repeat;
   img {
@@ -50,43 +50,45 @@ export default {
     height: 100%;
   }
   .center {
-    display:flex;
+    display: flex;
     position: absolute;
     bottom: 225px;
-    width:100%;
-    margin:auto;
-    padding:10px 161px;
-    align-items:flex-end;
-    justify-content:space-between;
+    width: 100%;
+    margin: auto;
+    padding: 10px 161px;
+    align-items: flex-end;
+    justify-content: space-between;
     box-sizing: border-box;
     .video-box {
       background: url(../assets/img/class/jx.png) 100% 100% / 100% 100%
         no-repeat;
       padding: 30px;
-      width:780px;
-      height:469px;
+      width: 780px;
+      height: 469px;
       .video {
         width: 100%;
         height: 100%;
       }
     }
-    .right{
+    .right {
       padding: 30px;
-      width:884px;
-      height:410px;
-      padding-top:200px;
-      background: url(../assets/img/class/class.png) 100% 100% / 100% 100% no-repeat;
-      .box-text{
+      width: 884px;
+      height: 410px;
+      padding-top: 200px;
+      background: url(../assets/img/class/class.png) 100% 100% / 100% 100%
+        no-repeat;
+      .box-text {
+        text-justify: inter-ideograph;
         letter-spacing: 5px;
         text-indent: 100px;
         width: 91%;
         font-size: 32px;
-        line-height:41px;
-        text-indent:40px;
-        color:#fff;
+        line-height: 41px;
+        text-indent: 40px;
+        color: #fff;
         margin: auto;
         line-height: 40px;
-        text-align:left;
+        text-align: left;
       }
     }
     .text-box {
@@ -103,7 +105,7 @@ export default {
         letter-spacing: 5px;
         text-indent: 100px;
         width: 91%;
-        font-size: 36px;
+        font-size: 38px;
         color: #49f4fe;
         margin: auto;
         line-height: 40px;
--- src/views/newteam.vue
+++ src/views/newteam.vue
@@ -11,7 +11,8 @@
       <div class="text-box">
         <div class="box-text">
           {{ text }}
-        </div>>
+        </div>
+        >
       </div>
     </div>
   </div>
@@ -22,7 +23,8 @@ export default {
   name: 'Newteam',
   data () {
     return {
-      text: '课程主要面对从事电视会议系统执勤维护工作的相关人员，针对当前典型电视会议系统设备操作训练需求，包含原理示教、装备操作、作战运用3个组训单元，22个训练科目，重点针对电视会议系统功能、模型、接口、线缆等县相关基础知识、主流电视会议系统的结构组成及工作原理、典型设备操作和配置方法以及组网运用方法进行训练。培养执勤维护人员具备对应岗位职责所需的专业素养、满足信息通信保障任务要求。'
+      text:
+        '教学团队长期从事电视会议系统一线教学及科研工作，具有丰富的教学经验与雄厚的科研能力，团队成员不仅擅长电视会议系统设备教学、操作、组网运用，而且先后参加了全军电视会议系统整冶升级论证与建设工作，并参与完成了全军电视会议室标准拟制工作，在全军电视会议系统构建和相关领域具有显著的优势，在军内具备较强的影响力，多名教员多次应邀担任电视会议系统相关竞赛评委。'
     }
   },
   components: {}
@@ -32,12 +34,11 @@ export default {
 <style lang="less" scope>
 @bg: #1f1f1f;
 .warp {
-  height:100%;
+  height: 100%;
   width: 100%;
   position: relative;
-  background:url('../assets/img/class/bg(1).png') no-repeat;
-  background-size:cover
-  img {
+  background: url('../assets/img/class/bg(1).png') no-repeat;
+  background-size:cover img {
     width: 100%;
     height: 100%;
   }
@@ -45,11 +46,11 @@ export default {
     position: absolute;
     bottom: 160px;
     padding: 40px;
-    left:320px;
-    width:1000px;
-    height:500px;
-    overflow:hidden;
-    box-sizing:border-box;
+    left: 320px;
+    width: 1000px;
+    height: 500px;
+    overflow: hidden;
+    box-sizing: border-box;
     .text-box {
       text-align: left;
 
@@ -58,7 +59,7 @@ export default {
         text-indent: 100px;
         width: 91%;
         font-size: 32px;
-        color:#ffea00;
+        color: #ffea00;
         margin: auto;
         line-height: 42px;
       }
